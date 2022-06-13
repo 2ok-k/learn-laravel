07f5c8b86b8bfaba94e571e5ea56b0045a3fbc3f
2ok-k <52870159+2ok-k@users.noreply.github.com>
2022-06-13 18:20:36 +0000

    Eloquent en cours...

---
+++ app/Models/Comment.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    //Nous avons un commentaire qui appartient à un seul pôst
+    public function post(){
+        return $this->belongsTo(Post::class);
+    }
+}
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,14 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Comment;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
     use HasFactory;
 
     protected $fillable = ['title','content'];//Je veux que tu remplisses "title" et "content" quand je te le demande
+
+    //Nous avons un post qui detient plusieurs commentaires
+    public function comments(){
+        return $this->hasMany(Comment::class);
+    }
 
     //protected $primaryKey = 'post_id';//Rédefinir le id s'il yavait pas d'ID
 }
