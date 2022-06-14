2b73dc6a5adea26f6f3dcccb586c35df40bf59eb
2ok-k <52870159+2ok-k@users.noreply.github.com>
2022-06-14 08:33:59 +0000

    eloquent : relation One To One

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Image;
 use App\Models\Comment;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -15,6 +16,11 @@ class Post extends Model
     //Nous avons un post qui detient plusieurs commentaires
     public function comments(){
         return $this->hasMany(Comment::class);
+    }
+
+    //Un post à une et une seule image
+    public function image(){
+        return $this->hasOne(Image::class);
     }
 
     //protected $primaryKey = 'post_id';//Rédefinir le id s'il yavait pas d'ID
--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -2,12 +2,13 @@
 
 @section('content')
     <h1>{{  $post -> content }}</h1>
+    <span>{{ $post -> image ? $post -> image -> path : "pas d'image" }}</span>
     <div style="border-top: 1px solid #000;width : 700px;"></div>
     {{--@foreach ($post -> comments as $comment) 
         <h4 style="margin-left: 10%">{{ $comment -> content }}</h4>
     @endforeach--}}
     @forelse ($post -> comments as $comment) 
-        <h4 style="margin-left: 10%">{{ $comment -> content }}</h4>
+        <h4 style="margin-left: 10%">{{ $comment -> content}}</h4>
     @empty
         <h4 style="margin-left: 10%">Aucun commentaire pour ce post.</h4>
     @endforelse
